9ea2381e119f264bd7402abdf28634224d8df60c
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2022-06-29 16:53:37 +0200

    userdata.py: fix pylint warnings
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- src/userdata.py
+++ src/userdata.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+"""Generate cloud-init user-data"""
 
 import argparse
-from pathlib import Path
 import yaml
 
 class UserData:
-    def __init__(self, hostName, sshKeyFile, user):
-        self.data = {};
-        self.data['hostname'] = hostName
+    """Generate cloud-init user-data"""
+
+    def __init__(self, host_name, ssh_key_file, user):
+        self.data = {}
+        self.data['hostname'] = host_name
         self.data['manage_etc_hosts'] = 'localhost'
         self.data['apt'] = {'sources' : {
             'rvrelease' : { 'source' : 'ppa:ubuntu-risc-v-team/release' },
@@ -18,16 +20,21 @@ class UserData:
             'shell' : '/bin/bash',
             'groups' : 'dialout, kvm, sudo',
             'sudo' : 'ALL=(ALL) NOPASSWD:ALL',
-            'ssh_authorized_keys' : self.readKeyFile(sshKeyFile)
+            'ssh_authorized_keys' : self.read_key_file(ssh_key_file)
         }]
 
-    def dump(self, fileName):
-        with open(fileName, 'w', encoding='utf-8') as file:
+    def dump(self, file_name):
+        """Writes the user-data yaml file"""
+
+        with open(file_name, 'w', encoding='utf-8') as file:
             file.write('#cloud-config\n')
             file.write(yaml.dump(self.data))
 
-    def readKeyFile(self, fileName):
-        with open(fileName, 'r', encoding='utf-8') as file:
+    @staticmethod
+    def read_key_file(file_name):
+        """Reads the SSH key file"""
+
+        with open(file_name, 'r', encoding='utf-8') as file:
             return file.read().strip().splitlines()
 
 def main():
@@ -38,8 +45,8 @@ def main():
     parser.add_argument('-s', '--sshkeyfile', default='id_rsa.pub', help='ssh key file')
     parser.add_argument('-u', '--user', default='user', help='user name')
     args = parser.parse_args()
-    ud = UserData(args.hostname, args.sshkeyfile, args.user)
-    ud.dump(args.filename)
+    user_data = UserData(args.hostname, args.sshkeyfile, args.user)
+    user_data.dump(args.filename)
 
 if __name__ == '__main__':
     main()
