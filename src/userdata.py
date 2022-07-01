baf40ee6498da9b88f416fe53e63d7eb274e37be
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2022-07-01 11:39:28 +0200

    userdata.py: allow specifying packages and reboot
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- src/userdata.py
+++ src/userdata.py
@@ -7,7 +7,7 @@ import yaml
 class UserData:
     """Generate cloud-init user-data"""
 
-    def __init__(self, host_name, ssh_key_file, user):
+    def __init__(self, host_name, ssh_key_file, user, packages = '', reboot = False):
         self.data = {}
         self.data['hostname'] = host_name
         self.data['manage_etc_hosts'] = 'localhost'
@@ -35,6 +35,10 @@ class UserData:
             'sudo' : 'ALL=(ALL) NOPASSWD:ALL',
             'ssh_authorized_keys' : self.read_key_file(ssh_key_file)
         }]
+        self.data['packages'] = packages.split()
+        self.data['package_update'] = True
+        if reboot:
+            self.data['runcmd'] = ['reboot']
 
     def dump(self, file_name):
         """Writes the user-data yaml file"""
@@ -55,10 +59,12 @@ def main():
     parser = argparse.ArgumentParser(description='Create cloud-init user-data')
     parser.add_argument('-n', '--hostname', default='unknown', type=str, help='hostname')
     parser.add_argument('-o', '--filename', default='user-data', type=str, help='file name')
+    parser.add_argument('-p', '--packages', default='user-data', type=str, help='whitespace separated list of packages')
     parser.add_argument('-s', '--sshkeyfile', default='id_rsa.pub', help='ssh key file')
+    parser.add_argument('-r', '--reboot', action='store_true', help='reboot')
     parser.add_argument('-u', '--user', default='user', help='user name')
     args = parser.parse_args()
-    user_data = UserData(args.hostname, args.sshkeyfile, args.user)
+    user_data = UserData(args.hostname, args.sshkeyfile, args.user, args.packages, args.reboot)
     user_data.dump(args.filename)
 
 if __name__ == '__main__':
